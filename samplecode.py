60712033f090dbbebaf1e45bbfaefc0564ebdefa
AbhiEdara143 <155036526+AbhiEdara143@users.noreply.github.com>
2024-02-03 15:35:16 -0600

    multiplication of 9

---
+++ samplecode.py
@@ -0,0 +1,7 @@
+def print_multiplication_table(n):
+    for i in range(1, 11):
+        result = n * i
+        print(f"{n} * {i} = {result}")
+
+# Call the function to print the 9 table
+print_multiplication_table(9)
